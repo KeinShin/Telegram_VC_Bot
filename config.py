5372db08dd15a437010fd013afd71fbc32e0d169
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-26 15:27:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 bot_token = "1430367736:AAEXlPz5JSRBGaXzXilNM_EUFdJvJqw-gCg"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001255782872
+sudo_chat_id =  -1001291663564
 owner_id = 1311769691
